208a6e288684e94213dad01a8f91f0d798e44677
Max Smolens <max.smolens@kitware.com>
2018-08-01 16:25:15 -0400

    Add validation and creation method to WorkingSet model

--- danesfield-server/server/models/workingSet.py
+++ danesfield-server/server/models/workingSet.py
@@ -1,3 +1,4 @@
+from girder.exceptions import ValidationException
 from girder.models.model_base import Model
 
 
@@ -9,4 +10,27 @@ class WorkingSet(Model):
         self.ensureIndex('parentWorkingSetId')
 
     def validate(self, model):
+        # Name must not be empty
+        if not model['name']:
+            raise ValidationException('Name must not be empty.', 'name')
+
+        # Dataset IDs must be a list
+        if not isinstance(model['datasetIds'], list):
+            raise ValidationException('Dataset IDs must be a list.', 'datasetIds')
+
         return model
+
+    def createWorkingSet(self, name, parentWorkingSet=None, datasetIds=[], filterId=None):
+        """
+        Create a new working set.
+        """
+        doc = {
+            'name': name,
+            'parentWorkingSetId': parentWorkingSet['_id'],
+            'datasetIds': datasetIds
+        }
+
+        if filterId:
+            doc['filterId'] = filterId
+
+        return self.save(doc)
