111e0f68461f9920980f3071173640f2fbf65360
David Joy <david.joy@admins-MacBook-Pro.local>
2018-09-26 23:35:27 -0400

    Removing workflow logic to skip run_metrics step if aoi not set

--- danesfield-server/server/workflow_manager.py
+++ danesfield-server/server/workflow_manager.py
@@ -27,8 +27,6 @@ from .constants import DanesfieldStep
 from .job_info import JobInfo
 from .models.workingSet import WorkingSet
 from .workflow import DanesfieldWorkflowException
-from .workflow_steps import (
-    RunMetricsStep)
 
 
 class DanesfieldWorkflowManager(object):
@@ -290,12 +288,12 @@ class DanesfieldWorkflowManager(object):
             ]
 
             # Skip run-metrics if the AOI is unknown
-            model = jobData['options'].get('classify-materials', {}).get('model')
-            if model is None or model == 'STANDARD':
-                try:
-                    incompleteSteps.remove(RunMetricsStep)
-                except ValueError as e:
-                    pass
+            # model = jobData['options'].get('classify-materials', {}).get('model')
+            # if model is None or model == 'STANDARD':
+            #     try:
+            #         incompleteSteps.remove(RunMetricsStep)
+            #     except ValueError as e:
+            #         pass
 
             logprint.info('DanesfieldWorkflowManager.advance IncompleteSteps={}'.format(
                 [step.name for step in incompleteSteps]
