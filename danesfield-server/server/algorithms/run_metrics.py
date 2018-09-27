25fff3fe759fd139efa5aef3d664a800bf1f2ef2
David Joy <david.joy@admins-MacBook-Pro.local>
2018-09-26 23:47:00 -0400

    Add in the run_metrics workflow step and algorithm

---
+++ danesfield-server/server/algorithms/run_metrics.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+##############################################################################
+
+from girder_worker.docker.tasks import docker_run
+from girder_worker.docker.transforms import VolumePath
+from girder_worker.docker.transforms.girder import (
+    GirderFileIdToVolume,
+    GirderUploadVolumePathToFolder,
+    GirderFolderIdToVolume)
+
+from .common import addJobInfo, createDockerRunArguments, createGirderClient, createUploadMetadata
+from ..constants import DockerImage
+
+
+def runMetrics(initWorkingSetName,
+               stepName,
+               requestInfo,
+               jobId,
+               outputFolder,
+               referenceFolder,
+               referencePrefix,
+               dtmFile,
+               dsmFile,
+               clsFile,
+               mtlFile):
+    """
+    Run a Girder Worker job to compute metrics on output files.
+
+    Requirements:
+    - Danesfield Docker image is available on host
+
+    :param initWorkingSetName: The name of the top-level working set.
+    :type initWorkingSetName: str
+    :param stepName: The name of the step.
+    :type stepName: str (DanesfieldStep)
+    :param requestInfo: HTTP request and authorization info.
+    :type requestInfo: RequestInfo
+    :param jobId: Job ID.
+    :type jobId: str
+    :param outputFolder: Output folder document.
+    :type outputFolder: dict
+    :param referenceFolder: Reference directory.
+    :type referenceFolder: dict
+    :param referencePrefix: Reference file prefix.
+    :type referencePrefix: str
+    :param dtmFile: DTM file document.
+    :type dtmFile: dict
+    :param dsmFile: DSM file document.
+    :type dsmFile: dict
+    :param clsFile: CLS file document.
+    :type clsFile: dict
+    :param mtlFile: MTL file document.
+    :type mtlFile: dict
+    :returns: Job document.
+    """
+    gc = createGirderClient(requestInfo)
+
+    outputVolumePath = VolumePath('__output__')
+
+    # Docker container arguments
+    containerArgs = [
+        'danesfield/tools/run_metrics.py',
+        '--output-dir', outputVolumePath,
+        '--ref-dir', GirderFolderIdToVolume(referenceFolder['_id'], gc=gc),
+        '--ref-prefix', referencePrefix,
+        '--dsm', GirderFileIdToVolume(dsmFile['_id'], gc=gc),
+        '--cls', GirderFileIdToVolume(clsFile['_id'], gc=gc),
+        '--mtl', GirderFileIdToVolume(mtlFile['_id'], gc=gc),
+        '--dtm', GirderFileIdToVolume(dtmFile['_id'], gc=gc)
+    ]
+
+    # Result hooks
+    # - Upload output files to output folder
+    # - Provide upload metadata
+    upload_kwargs = createUploadMetadata(jobId, stepName)
+    resultHooks = [
+        GirderUploadVolumePathToFolder(
+            outputVolumePath,
+            outputFolder['_id'],
+            upload_kwargs=upload_kwargs,
+            gc=gc)
+    ]
+
+    asyncResult = docker_run.delay(
+        **createDockerRunArguments(
+            image=DockerImage.DANESFIELD,
+            containerArgs=containerArgs,
+            jobTitle='[%s] Run metrics' % initWorkingSetName,
+            jobType=stepName,
+            user=requestInfo.user,
+            resultHooks=resultHooks
+        )
+    )
+
+    # Add info for job event listeners
+    job = asyncResult.job
+    job = addJobInfo(job, jobId=jobId, stepName=stepName)
+
+    return job
