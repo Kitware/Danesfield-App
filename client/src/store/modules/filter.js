5c27943ba0244ad5beb23c86b6dd0d36e03a7506
David Joy <david.joy@kitware.com>
2018-11-14 11:49:11 -0500

    Added license and copyright

--- client/src/store/modules/filter.js
+++ client/src/store/modules/filter.js
@@ -1,3 +1,9 @@
+/*#############################################################################
+# Copyright Kitware Inc. and Contributors
+# Distributed under the Apache License, 2.0 (apache.org/licenses/LICENSE-2.0)
+# See accompanying Copyright.txt and LICENSE files for details
+#############################################################################*/
+
 import pointOnFeature from '@turf/point-on-feature';
 
 import { loadDatasetByFilterConditions } from "../../utils/loadDataset";
--- client/src/utils/extraPalettes.js
+++ client/src/utils/extraPalettes.js
@@ -1,3 +1,9 @@
+/*#############################################################################
+# Copyright Kitware Inc. and Contributors
+# Distributed under the Apache License, 2.0 (apache.org/licenses/LICENSE-2.0)
+# See accompanying Copyright.txt and LICENSE files for details
+#############################################################################*/
+
 export const blueRed = ["blue", "red"];
 export const blueWhiteRed = ["blue", "white", "red"];
 export const blackWhite = ["black", "white"];
