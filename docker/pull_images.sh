125f4036640a0a67dd7368919ab0bee6b4e858d4
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-03-25 13:44:59 -0400

    Add login step to pull_images script

--- docker/pull_images.sh
+++ docker/pull_images.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Login
+docker login https://gitlab.kitware.com:4567/v2/core3d/danesfield-app 
+
 # Pull and Tag Danesfield Image
 docker pull kitware/danesfield
 docker tag kitware/danesfield:latest gitlab.kitware.com:4567/core3d/danesfield-app/danesfield:latest
