c6377aebfca2fcc55af793bf36e08ec5e4b3f579
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-03-30 17:14:24 -0400

    [WIP] Add conda deployment steps

---
+++ ansible/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Exit if any commands fail
+set -e
+
+# Initialize conda
+source $(conda info --base)/etc/profile.d/conda.sh
+
+# Env
+conda env create -f ansible/conda_environment.yml
+conda activate danesfield
+mkdir -p $CONDA_PREFIX/var/lib/mongodb
+mongod --fork --syslog --dbpath=$CONDA_PREFIX/var/lib/mongodb
+
+# girder
+pip install -e server/
+pip install -U pip setuptools
+
+# client
+git submodule update --init --recursive
+npm install -g yarn
+yarn --cwd client/ --frozen-lockfile
+yarn --cwd client/ build
+
+# Run girder build
+girder build
+
+# Copy static files
+cp -r client/dist $CONDA_PREFIX/share/girder/static/core3d
+
+# TEST
+conda deactivate
+conda env remove --name danesfield
