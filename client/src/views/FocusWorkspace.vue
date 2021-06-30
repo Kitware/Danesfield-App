eb213ec88b2af1559367f34169e7d634e4f65cc8
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-06-30 17:00:11 -0400

    Format

--- client/src/views/FocusWorkspace.vue
+++ client/src/views/FocusWorkspace.vue
@@ -5,91 +5,105 @@
 #############################################################################*/
 
 <template>
-<div class="root">
-  <WorkspaceContainer
-    :focused="focusedWorkspace"
-    @update:focused="setFocusedWorkspaceKey($event)"
-    :autoResize="true"
-    :max="2">
-    <Workspace
-      v-for="(workspace, key) in workspaces"
-      :key="key"
-      :identifier="key"
-      @split="addWorkspace(workspace)"
-      @close="removeWorkspace(key)"
-      :states="[{name:'Map',value:'map',disabled:workspace.type==='map'},{name:'3D View',value:'vtk',disabled:workspace.type==='vtk'}]"
-      @stateChange="changeWorkspaceType({workspace,type:$event})"
+  <div class="root">
+    <WorkspaceContainer
+      :focused="focusedWorkspace"
+      @update:focused="setFocusedWorkspaceKey($event)"
+      :autoResize="true"
+      :max="2"
+    >
+      <Workspace
+        v-for="(workspace, key) in workspaces"
+        :key="key"
+        :identifier="key"
+        @split="addWorkspace(workspace)"
+        @close="removeWorkspace(key)"
+        :states="[
+          { name: 'Map', value: 'map', disabled: workspace.type === 'map' },
+          { name: '3D View', value: 'vtk', disabled: workspace.type === 'vtk' },
+        ]"
+        @stateChange="changeWorkspaceType({ workspace, type: $event })"
       >
-      <GeojsMapViewport v-if="workspace.type==='map'" key="geojs-map"
-        class="map"
-        :viewport="viewport"
-        :zoomRange="{ min: 0, max: 18 }"
-        ref="geojsMapViewport">
-        <AdaptedColorLegendLayer
-          :layers="orderLayer(workspace.layers)"
-          :datasetIdMetaMap="datasetIdMetaMap" />
-        <GeojsTileLayer
-          url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
-          attribution='© OpenStreetMap contributors, © CARTO'
-          :zIndex='0'>
-        </GeojsTileLayer>
-        <template v-for="(layer,i) in orderLayer(workspace.layers)">
-          <GeojsGeojsonDatasetLayer
-            v-if="layer.dataset.geometa.driver==='GeoJSON'"
-            :key="layer.dataset._id"
-            :dataset="layer.dataset"
-            :summary="datasetIdMetaMap[layer.dataset._id].summary"
-            :geojson="datasetIdMetaMap[layer.dataset._id].geojson"
-            :opacity="layer.opacity"
-            :zIndex="i+1"
-            @click='layerClicked(layer, $event)'>
-          </GeojsGeojsonDatasetLayer>
-          <StyledGeoTIFFLayer
-            v-if="layer.dataset.geometa.driver==='GeoTIFF'"
-            :key="layer.dataset._id"
-            :dataset="layer.dataset"
-            :tileURL="getTileURL(layer.dataset)"
-            :opacity="layer.opacity"
-            :keepLower="false"
-            :zIndex="i+1"
-            @click='layerClicked(layer, $event)'>
-          </StyledGeoTIFFLayer>
-        </template>
-      </GeojsMapViewport>
-        <VTKViewport v-if="workspace.type==='vtk'"
-          :background="vtkBGColor">
+        <GeojsMapViewport
+          v-if="workspace.type === 'map'"
+          key="geojs-map"
+          class="map"
+          :viewport="viewport"
+          :zoomRange="{ min: 0, max: 18 }"
+          ref="geojsMapViewport"
+        >
+          <AdaptedColorLegendLayer
+            :layers="orderLayer(workspace.layers)"
+            :datasetIdMetaMap="datasetIdMetaMap"
+          />
+          <GeojsTileLayer
+            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+            attribution="© OpenStreetMap contributors, © CARTO"
+            :zIndex="0"
+          >
+          </GeojsTileLayer>
+          <template v-for="(layer, i) in orderLayer(workspace.layers)">
+            <GeojsGeojsonDatasetLayer
+              v-if="layer.dataset.geometa.driver === 'GeoJSON'"
+              :key="layer.dataset._id"
+              :dataset="layer.dataset"
+              :summary="datasetIdMetaMap[layer.dataset._id].summary"
+              :geojson="datasetIdMetaMap[layer.dataset._id].geojson"
+              :opacity="layer.opacity"
+              :zIndex="i + 1"
+              @click="layerClicked(layer, $event)"
+            >
+            </GeojsGeojsonDatasetLayer>
+            <StyledGeoTIFFLayer
+              v-if="layer.dataset.geometa.driver === 'GeoTIFF'"
+              :key="layer.dataset._id"
+              :dataset="layer.dataset"
+              :tileURL="getTileURL(layer.dataset)"
+              :opacity="layer.opacity"
+              :keepLower="false"
+              :zIndex="i + 1"
+              @click="layerClicked(layer, $event)"
+            >
+            </StyledGeoTIFFLayer>
+          </template>
+        </GeojsMapViewport>
+        <VTKViewport v-if="workspace.type === 'vtk'" :background="vtkBGColor">
           <OBJMultiItemActor
             v-for="layer in workspace.layers"
             v-if="isOBJItem(layer.dataset)"
             :key="layer.dataset._id"
             :item="layer.dataset"
             :texture="workspace.texture"
-             />
+          />
         </VTKViewport>
-      <template slot='actions' v-if="workspace.type==='vtk'">
-        <WorkspaceAction>
-          <v-menu
-            top offset-y
-            origin="center center">
+        <template slot="actions" v-if="workspace.type === 'vtk'">
+          <WorkspaceAction>
+            <v-menu top offset-y origin="center center">
+              <v-icon slot="activator">palette</v-icon>
+              <v-card width="130px">
+                <Palette
+                  :value="vtkBGColor"
+                  @input="changeVTKBGColor($event)"
+                />
+              </v-card>
+            </v-menu>
+          </WorkspaceAction>
+          <WorkspaceAction>
             <v-icon
-              slot="activator"
-            >palette</v-icon>
-            <v-card width="130px">
-              <Palette :value="vtkBGColor" @input="changeVTKBGColor($event)" />
-            </v-card>
-          </v-menu>
-        </WorkspaceAction>
-        <WorkspaceAction>
-          <v-icon :class="{'grey--text text--darken-1':!workspace.texture}" @click="workspace.texture=!workspace.texture">texture</v-icon>
-        </WorkspaceAction>
-      </template>
-    </Workspace>
-  </WorkspaceContainer>
-  <ClickInfoDialog
-    right="15px"
-    bottom="60px"
-    :datasetClickEvents="datasetClickEvents" />
-</div>
+              :class="{ 'grey--text text--darken-1': !workspace.texture }"
+              @click="workspace.texture = !workspace.texture"
+              >texture</v-icon
+            >
+          </WorkspaceAction>
+        </template>
+      </Workspace>
+    </WorkspaceContainer>
+    <ClickInfoDialog
+      right="15px"
+      bottom="60px"
+      :datasetClickEvents="datasetClickEvents"
+    />
+  </div>
 </template>
 
 <script>
@@ -126,7 +140,7 @@ export default {
     VTKViewport,
     OBJMultiItemActor,
     Palette,
-    ClickInfoDialog
+    ClickInfoDialog,
   },
   props: [
     "boundDatasets",
@@ -139,19 +153,19 @@ export default {
     "removeWorkspace",
     "changeWorkspaceType",
     "vtkBGColor",
-    "changeVTKBGColor"
+    "changeVTKBGColor",
   ],
   data() {
     return {
       isOBJItem,
-      datasetClickEvents: []
+      datasetClickEvents: [],
     };
   },
   computed: {
     viewport() {
       var viewPort = {
         center: [-95, 40],
-        zoom: 4
+        zoom: 4,
       };
       if (!this.boundDatasets || !this.boundDatasets.length) {
         return viewPort;
@@ -163,14 +177,14 @@ export default {
           return;
         }
         var eligibleDatasets = this.boundDatasets.filter(
-          dataset => dataset.geometa && dataset.geometa.bounds
+          (dataset) => dataset.geometa && dataset.geometa.bounds
         );
         if (!eligibleDatasets.length) {
           return;
         }
         var bboxOfAllDatasets = bbox(
           geometryCollection(
-            eligibleDatasets.map(dataset => dataset.geometa.bounds)
+            eligibleDatasets.map((dataset) => dataset.geometa.bounds)
           )
         );
         var dist = distance(
@@ -186,14 +200,14 @@ export default {
             left: bufferedBbox[0],
             right: bufferedBbox[2],
             top: bufferedBbox[3],
-            bottom: bufferedBbox[1]
+            bottom: bufferedBbox[1],
           });
         } catch (ex) {
           console.warn(ex.message);
           return viewPort;
         }
       }
-    }
+    },
   },
   created() {
     this.debounceSetdatasetClickEvents = debounce(() => {
@@ -219,9 +233,9 @@ export default {
     },
     orderLayer(layers) {
       var orderedDatasetIds = this.listingDatasetIdAndWorkingSets.map(
-        datasetIdAndWorkingSet => datasetIdAndWorkingSet.datasetId
+        (datasetIdAndWorkingSet) => datasetIdAndWorkingSet.datasetId
       );
-      return sortBy(layers, layer => {
+      return sortBy(layers, (layer) => {
         return -orderedDatasetIds.indexOf(layer.dataset._id);
       });
     },
@@ -229,12 +243,12 @@ export default {
       if (layer.opacity) {
         this.datasetClickEventsAggregator.push({
           dataset: layer.dataset,
-          clickEvent: clickEvent
+          clickEvent: clickEvent,
         });
         this.debounceSetdatasetClickEvents();
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
